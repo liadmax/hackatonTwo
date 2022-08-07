5173c9879d444008ae88f7b344ca0ae43dde71f7
unknown <shapiraliad@gmail.com>
2022-08-07 13:46:26 +0300

    first'

---
+++ script.js
@@ -0,0 +1,7 @@
+function checkInfo(e){
+    e.preventDefault();
+    let username = document.getElementById('userName').value;
+let password = document.getElementById('password').value;
+console.log(`this is the password ${password}`);
+console.log(`this is the username ${username}`);
+}
